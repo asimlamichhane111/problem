9a8b11cd92fda1d629782b25600b1d8d34d1abd7
asimlamichhane111 <asimlamichhane000@gmail.com>
2025-04-29 13:03:53 +0545

    Add files via upload

---
+++ orders/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('canceled', 'Canceled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)  # Link to logged-in user
+    customer_name = models.CharField(max_length=255)
+    customer_phone=models.CharField(max_length=15,blank=True,null=True)
+    order_date = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    @property
+    def total_price(self):
+        return sum(item.total_price for item in self.items.all())
+
+    def __str__(self):
+        return f"Order {self.id} - {self.customer_name}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey("inventory.Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} for Order {self.order.id}"
